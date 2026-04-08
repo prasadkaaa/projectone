0e2d924faf109798b5eae0fceb30f22894e09948
prasadkaaa <prasadkambala100@gmail.com>
2026-04-08 22:05:07 +0530

    Update H1 and add Jenkinsfile

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(500);
+            res.end('Error loading file');
+            return;
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(content);
+    });
+}).listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
