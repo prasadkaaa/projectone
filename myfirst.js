7a32d810a4558af65491bab4858a36a324205fc4
prasadkaaa <prasadkambala100@gmail.com>
2026-04-15 08:21:22 +0530

    added filesystemlearning

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,21 @@
 
 const fs =  require('fs')
-const content = fs.readFileSync('notes.txt', 'utf-8')
-fs.appendFileSync('copy.txt',`\n\n ${content}`,'utf-8')
-console.log(content);
-fs.unlinkSync('copy.txt')
+//const content = fs.readFileSync('notes.txt', 'utf-8')
+//fs.appendFileSync('copy.txt',`\n\n ${content}`,'utf-8')
+//console.log(content);
+
+console.log("start of script");
+
+//non blocking
+fs.readFile('notes.txt', 'utf-8',function(err,data){
+    if(err) console.log(err)
+        else console.log(data)
+})
+
+// blocking
+
+// const content = fs.readFileSync('notes.txt', 'utf-8');
+// console.log(content);
+
+console.log(" end of script");
+
