b1d984abdda13bab15d0f54f8661df4ec482403d
prasadkaaa <prasadkambala100@gmail.com>
2026-04-16 18:11:04 +0530

    added express crud

--- 02_express_BookStore/index.js
+++ 02_express_BookStore/index.js
@@ -10,6 +10,9 @@ const books = [
     { id: 3, title: 'Book THree', author: 'Author three' }
 ]
 
+//MiddleWare( PLugins)
+app.use(express.json());
+
 //Routes
 app.get('/books', function (req, res) {
     res.json(books);
@@ -18,7 +21,7 @@ app.get('/books', function (req, res) {
 app.get('/books/:id', function (req, res) {
     const ID= parseInt(req.params.id);
     if(isNaN(ID)) return res.status(400).json({error:` id must be type number`});
-    
+
     const book = books.find(e => e.id == ID) // like select * from books where id is xx
 
     if(!book){
@@ -26,6 +29,30 @@ app.get('/books/:id', function (req, res) {
     }
     return  res.json(book);
 })  
+
+app.post('/books',(req,res) =>{
+     const {title, author} = req.body;
+     if(!title || title === '') return  res.status(400).json({error:`title is required`});
+     if(!author || author === '') return  res.status(400).json({error:`author is required`});
+
+     const id = (books.length + 1);
+
+     const book ={id,title,author};
+     books.push(book);
+    return res.status(201).json({message:` Book created successfully ${id}`});
+})
+
+app.delete('/books/:id', function (req, res) {
+    const ID= parseInt(req.params.id);
+    if(isNaN(ID)) return res.status(400).json({error:` id must be type number`});
+
+    const indexToDelete = books.findIndex(e => e.id == ID) // like delete  from books where id is xx
+
+    if(indexToDelete < 0) return res.status(404).json({error:`Book with id ${ID} does not exist`});
+    
+   books.splice(indexToDelete,1);
+   return res.status(200).json({message:` book got deleted`});
+}) 
 
 
 
