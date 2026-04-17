d626243d63b19daa0abc4bb88780de70c477d1ed
prasadkaaa <prasadkambala100@gmail.com>
2026-04-17 13:58:10 +0530

    added userdefined module

---
+++ UserDefineModule/index.js
@@ -0,0 +1,10 @@
+const {add,sub,mul,div} = require('./math');
+
+
+console.log(add(12,12));
+
+console.log(sub(12,6));
+
+console.log(mul(12,12));
+
+console.log(div(12,2));
---
+++ UserDefineModule/math.js
@@ -0,0 +1,20 @@
+exports.add = function add(a,b){
+    return a + b;
+}
+
+exports.sub = function sub(a,b){
+    return a - b;
+}
+
+exports.mul =function mul(a,b){
+    return a * b;
+}
+
+
+exports.div =function div(a,b){
+    return a / b;
+}
+
+
+// 1.named exports 
+// 2.default exports : there can be only one default export in one module, it does not have any  name like named export
