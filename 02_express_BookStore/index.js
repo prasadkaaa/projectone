16dbbf26a02866c8818749df926c5d62e86b8e1c
prasadkaaa <prasadkambala100@gmail.com>
2026-04-17 15:11:57 +0530

    add git ingnore

--- 02_express_BookStore/index.js
+++ 02_express_BookStore/index.js
@@ -1,22 +1,17 @@
 const express = require('express');
-const bookRouter = require('../routes/book.route')
 const app = express();
 const PORT = 8000;
 
 const {loggerMiddleWare} = require('../middleware/logger');
+const bookRouter = require('../routes/book.route')
 
 
 
 //MiddleWare( PLugins)
 app.use(express.json());
+app.use(loggerMiddleWare);
 
 //Routes
 app.use('/books',bookRouter);  // imported from route module folder
-
-
-
-
-
-
 
 app.listen(PORT, () => console.log(` server is runing on port ${PORT}`));
