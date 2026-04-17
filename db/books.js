5ee688bb3da999f3572b84ae769fe8051ca14b72
prasadkaaa <prasadkambala100@gmail.com>
2026-04-17 15:35:40 +0530

    deleted db folder

--- db/books.js
+++
@@ -1,5 +0,0 @@
-exports.BOOKS  = [
-    { id: 1, title: 'Book One', author: 'Author One' },
-    { id: 2, title: 'Book Two', author: 'Author Two' },
-    { id: 3, title: 'Book THree', author: 'Author three' }
-]
